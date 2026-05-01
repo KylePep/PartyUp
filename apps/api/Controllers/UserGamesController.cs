5390458f5f5ea050ee159c59b4a94d5f807a3045
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2026-04-30 21:49:48 -0600

    UserGames delete route

--- apps/api/Controllers/UserGamesController.cs
+++ apps/api/Controllers/UserGamesController.cs
@@ -1,3 +1,4 @@
+using System.Security.Claims;
 using Microsoft.AspNetCore.Mvc;
 using PartyUp.Api.Models;
 
@@ -29,5 +30,21 @@ public class UserGamesController : ControllerBase
     var games = await _service.GetUserGames(userId);
 
     return Ok(games);
+  }
+
+  [HttpDelete("{id}")]
+  public async Task<IActionResult> DeleteUserGame(Guid id)
+  {
+    var userId = Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!);
+
+    var deleted = await _service.DeleteUserGame(
+      id,
+      userId
+    );
+
+    if (!deleted)
+      return NotFound();
+
+    return NoContent();
   }
 }
--- apps/api/Services/Interfaces/IUserGameService.cs
+++ apps/api/Services/Interfaces/IUserGameService.cs
@@ -4,4 +4,5 @@ public interface IUserGameService
 {
   Task AddGameToUser(Guid userId, Game game);
   Task<List<Game>> GetUserGames(Guid userId);
+  Task<bool> DeleteUserGame(Guid id, Guid userId);
 }
