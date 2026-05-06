0e068b130e2871f498829caec02a0bb83b6ab849
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2026-05-05 22:11:42 -0600

    test: remove stale smoke test that called deleted /api/users endpoint

--- apps/tests/PartyUp.Api.Tests/Factories/ApiFactory.cs
+++ apps/tests/PartyUp.Api.Tests/Factories/ApiFactory.cs
@@ -6,20 +6,3 @@ namespace PartyUp.Api.Tests.Factories;
 public class ApiFactory : WebApplicationFactory<Program>
 {
 }
-
-public class SmokeTests : IClassFixture<ApiFactory>
-{
-  private readonly HttpClient _client;
-
-  public SmokeTests(ApiFactory factory)
-  {
-    _client = factory.CreateClient();
-  }
-
-  [Fact]
-  public async Task Health_check_should_work()
-  {
-    var response = await _client.GetAsync("/api/users");
-    response.EnsureSuccessStatusCode();
-  }
-}
