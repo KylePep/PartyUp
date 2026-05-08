2da145e7225f54821c85f2b91ddd9fa09d082e85
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2026-05-08 17:44:05 -0600

    fixed ai get games, inverted detail and simple get games

---
+++ apps/api/Models/DTOs/Game/GameSimple.cs
@@ -0,0 +1,10 @@
+using System.Text.Json.Serialization;
+
+namespace PartyUp.Api.Models.DTOs.Game;
+
+public class GameSimple
+{
+  public int ExternalId { get; set; }
+  public string Name { get; set; } = string.Empty;
+  public string? ImageUrl { get; set; }
+}
