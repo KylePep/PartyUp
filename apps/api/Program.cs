28ad8e0cfeb9eab0678e71396857fe32cc1d31b0
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2026-05-10 13:41:47 -0600

    feat: run EF migrations on startup

--- apps/api/Program.cs
+++ apps/api/Program.cs
@@ -110,6 +110,12 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 #region Middleware
 
 if (app.Environment.IsDevelopment())
